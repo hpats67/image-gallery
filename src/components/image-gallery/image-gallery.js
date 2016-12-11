849f6a9596ed0f239e85f2ecf4504a9695881d56
Steven Jackson <hpats67@gmail.com>
2016-12-10 19:51:59 -0800

    creation and working app

---
+++ src/components/image-gallery/image-gallery.js
@@ -0,0 +1,8 @@
+import template from './image-gallery.html'; 
+
+export default {
+  template,
+  bindings: {
+    image: '='
+  }
+};
---
+++ src/components/image-thumbnail/image-thumbnail.js
@@ -0,0 +1,9 @@
+
+import template from './image-thumbnail.html';
+
+export default {
+  template,
+  bindings: {
+    image: '='
+  }
+};
