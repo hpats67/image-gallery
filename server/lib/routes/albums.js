0646bf9146434d194d0a63678ed86f47abb24a61
Steven Jackson <hpats67@gmail.com>
2017-01-12 13:11:36 -0800

    Set up routes in albums to allow for retrieval of images in certain albums

--- server/lib/routes/albums.js
+++ server/lib/routes/albums.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser').json();
-const Album = require('../models/album');
+const Album = require('../models/albums');
+const Image = require('../models/image');
 
 router  
   .get('/', (req, res, next) => {
     Album.find({}).lean()
     .then(albums => res.send(albums))
+    .catch(next);
+  })
+
+  .get('/:id', (req, res, next) => {
+    const album = req.params.id;
+
+    Promise.all([
+      Album.findById(album).lean(),
+      Image.find({album}).lean()
+    ])
+    .then(([album, images]) => {
+      album.images = images;
+      res.send(album);
+    })
     .catch(next);
   })
 
